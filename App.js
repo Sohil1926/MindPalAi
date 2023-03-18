a80353762a494763586b40f341b4bc06a945cb23
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2023-03-18 13:09:46 -0400

    added react native elements

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@rneui/themed';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+ <Button title="Solid" />
+
     </View>
   );
 }
