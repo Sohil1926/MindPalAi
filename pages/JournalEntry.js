80ac41be0fb53540ac2c34ce98a29f784de47aae
Andrew Yang <andy.tubeee@gmail.com>
2023-04-02 18:10:45 -0400

    delete journal entries

--- pages/JournalEntry.js
+++ pages/JournalEntry.js
@@ -1,35 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, Alert } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { Button, Input } from '@rneui/themed';
 import axios from 'axios';
 import qs from 'qs';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
 // import { createCompletion } from './openAI';
-export default function JournalArchive({navigation, route}) {
-    const [JournalEntry, setJournalEntry] = useState('');
-    
+export default function JournalArchive({ navigation, route }) {
+  const [JournalEntry, setJournalEntry] = useState('');
 
-    const getJournalEntry = async () => {
-        try {
-            const value = await AsyncStorage.getItem(route.params.key)
-            setJournalEntry(value) 
-          } catch(e) {
-            // error reading value
-          }
-      };
+  const getJournalEntry = async () => {
+    try {
+      const value = await AsyncStorage.getItem(route.params.key);
+      setJournalEntry(value);
+    } catch (e) {
+      // error reading value
+      Alert.alert(
+        'Error',
+        'Something unexpected happened. Please try again later.'
+      );
+    }
+  };
 
-      useEffect(() => {
-        getJournalEntry();
-      });
+  const delEntry = async () => {
+    try {
+      await AsyncStorage.removeItem(route.params.key);
+      navigation.navigate('JournalArchive');
+    } catch (e) {
+      // error reading value
+      Alert.alert(
+        'Error',
+        'Something unexpected happened. Please try again later.'
+      );
+    }
+  };
+
+  useEffect(() => {
+    getJournalEntry();
+  });
 
   return (
     <View className='flex-1 justify-top py-20 gap-5 bg-black'>
-   <Text> 
-   {JournalEntry}
-    </Text>
+      <Text>{JournalEntry}</Text>
+      <Button title={'Delete'} onPress={delEntry} />
     </View>
   );
 }
