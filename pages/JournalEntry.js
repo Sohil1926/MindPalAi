e4451f2ab9aa80b20ba7cba2f04b208d33c6497f
Andrew Yang <andy.tubeee@gmail.com>
2023-04-15 17:15:28 -0400

    journal cover generation pipeline

--- pages/JournalEntry.js
+++ pages/JournalEntry.js
@@ -9,10 +9,13 @@ import {
   deleteValueFromArr,
   getValueFromKey,
 } from '../utils/asyncStorageUtils';
+import PillButton from '../components/PillButton';
+import { Image } from '@rneui/base';
 
 // import { createCompletion } from './openAI';
 export default function JournalArchive({ navigation, route }) {
   const [journalEntry, setJournalEntry] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
 
   const getJournalEntry = async () => {
     try {
@@ -27,6 +30,34 @@ export default function JournalArchive({ navigation, route }) {
       // error reading value
       Alert.alert('Error', 'Something unexpected happened. Please try again.');
     }
+  };
+
+  const generateImage = async () => {
+    setImageUrl('loading');
+    let data = qs.stringify({
+      input: `This is my's journal entry: "${journalEntry}"
+      I want an image that represents my mood.`,
+    });
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://openaibackend-nt11.onrender.com/dalle',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        setImageUrl(response.data.response);
+      })
+      .catch((error) => {
+        Alert.alert('Error', String(error));
+        console.error(error);
+      });
   };
 
   const delEntry = async () => {
@@ -48,8 +79,27 @@ export default function JournalArchive({ navigation, route }) {
 
   return (
     <View className='flex-1 justify-top py-20 gap-5 bg-black'>
-      <Text className='text-white'>{journalEntry}</Text>
-      <Button title={'Delete'} onPress={delEntry} />
+      <Text className='text-white mx-5'>{journalEntry}</Text>
+      <View className='flex flex-1 gap-4'>
+        <PillButton
+          text={'Show my mood'}
+          onPress={generateImage}
+          textColor='white'
+          bgColor='#1E5945'
+        />
+        <PillButton
+          text={'Delete'}
+          onPress={delEntry}
+          textColor='white'
+          bgColor='#ff0f00'
+        />
+      </View>
+      {imageUrl === 'loading' && (
+        <Text>loading your mood as a painting...</Text>
+      )}
+      {imageUrl !== '' && imageUrl !== 'loading' && (
+        <Image source={{ uri: imageUrl }} style={{ width: 200, height: 200 }} />
+      )}
     </View>
   );
 }
