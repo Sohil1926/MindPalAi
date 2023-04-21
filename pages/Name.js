e06e7598f37b311b4bc0946fddb6b7025ebf1fe9
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2023-04-20 21:03:07 -0400

    finished name page

--- pages/Name.js
+++ pages/Name.js
@@ -1,76 +1,105 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView } from 'react-native';
 import { Button } from '@rneui/base';
 import {
   useFonts,
   Manrope_800ExtraBold,
   Manrope_400Regular,
+  Manrope_200ExtraLight,
+  Manrope_300Light,
+  Manrope_500Medium,
+  Manrope_600SemiBold, 
+  Manrope_700Bold, 
 } from '@expo-google-fonts/manrope';
 import PillButton from '../components/PillButton';
 
 const Name = ({ navigation, setShowOnboarding }) => {
   const goToHome = () => {
-    alert("zzzz");
     navigation.navigate('Home');
   };
 
   const [fontsLoaded] = useFonts({
     Manrope_800ExtraBold,
     Manrope_400Regular,
+    Manrope_200ExtraLight,
+    Manrope_300Light,
+    Manrope_500Medium,
+    Manrope_600SemiBold, 
+    Manrope_700Bold, 
   });
+
+  const [textEntered, setTextEntered] = useState(false);
 
   if (!fontsLoaded) return null;
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.heading}>MindPal</Text>
+      <View style={styles.topSection}>
+        <Text style={styles.heading}>MindPal.</Text>
         <Text style={styles.subHeading}>let’s get started, what’s your name?</Text>
-        <TextInput style={styles.input} placeholder="Enter your name" />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your name"
+          onChangeText={(text) => setTextEntered(text.length > 0)}
+          underlineColorAndroid="transparent"
+        />
+      </View>
+      <View style={styles.bottomSection}>
         <PillButton
-          text='Get Started'
+          text="continue"
           onPress={goToHome}
-          bgColor={'#F4B400'}
-          textColor='white'
+          disabled={!textEntered}
+          bgColor={textEntered ? '#ffffff' : '#333333'}
+          textColor={textEntered ? '#333333' : '#ffffff'}
         />
       </View>
     </View>
   );
+  
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
+    container: {
+      flex: 1,
+      backgroundColor: 'black',
+      paddingTop: 50, // add this to push the content down
+      paddingHorizontal: 20, // add this for some horizontal padding
+    },
+    topSection: {
+      flex: 1, // add this to take up remaining space
+      justifyContent: 'flex-start', // add this to align content to top
+      alignItems: 'center', // center horizontally
+    },
+    heading: {
+      fontSize: 20,
+      marginTop: 30,
+      marginBottom: 20,
+      color: 'white',
+      fontFamily: 'Manrope_800ExtraBold',
+      textAlign: 'center',
+    },
+    subHeading: {
+      fontSize: 20,
+      paddingBottom: 20,
+      color: 'white',
+      fontFamily: 'Manrope_600SemiBold',
+      textAlign: 'center',
+    },
+    input: {
+      fontSize: 35,
+      fontFamily: 'Manrope_800ExtraBold',
+      color: 'white',
+      backgroundColor: '#020202',
+      borderRadius: 10,
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      textAlign: 'center',
+      marginBottom: 20,
+    },
+    bottomSection: {
+      marginBottom: 50, // add this to create space for the button
+    },
+  });
   
-  },
-  heading: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginTop: 30,
-    marginBottom: 20,
-    color: 'white',
-    fontFamily: 'Manrope_800ExtraBold',
-    textAlign: 'center',
-  },
-  subHeading: {
-    fontSize: 24,
-    fontWeight: 'normal',
-    paddingBottom: 20,
-    color: 'white',
-    fontFamily: 'Manrope_400Regular',
-    textAlign: 'center',
-  },
-  input: {
-    fontSize: 35,
-    fontWeight: 'normal',
-    backgroundColor: '#ffffff',
-    borderRadius: 10,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-});
 
 export default Name;
