9ea7e73e6be3c561c700d53694496c932037a627
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2023-04-20 20:03:27 -0400

    updated name input

--- pages/Name.js
+++ pages/Name.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { Button } from '@rneui/base';
 import {
   useFonts,
@@ -9,7 +9,8 @@ import {
 import PillButton from '../components/PillButton';
 
 const Name = ({ navigation, setShowOnboarding }) => {
-  const goToHome = () => { alert("zzzz");   
+  const goToHome = () => {
+    alert("zzzz");
     navigation.navigate('Home');
   };
 
@@ -23,27 +24,9 @@ const Name = ({ navigation, setShowOnboarding }) => {
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.heading}>we’re only going to show this once. </Text>
-        <Text style={styles.text}>
-          think of this app like BeReal but for journalling.{' '}
-        </Text>
-        <Text style={styles.text}>
-          its going to prompt you randomly 3 times a day to type how ur feeling
-          in.{' '}
-        </Text>
-        <Text style={styles.text}>
-          think of it like writing a tweet or smth
-        </Text>
-        <Text style={styles.text}>
-          you’ll have a assistant to help you write till you have explored your
-          thoughts{' '}
-        </Text>
-        <Text style={styles.text}>
-          each time you submit you build your streak
-        </Text>
-        <Text style={styles.text}>
-          all the stuff you write is stored locally on your device.{' '}
-        </Text>
+        <Text style={styles.heading}>MindPal</Text>
+        <Text style={styles.subHeading}>let’s get started, what’s your name?</Text>
+        <TextInput style={styles.input} placeholder="Enter your name" />
         <PillButton
           text='Get Started'
           onPress={goToHome}
@@ -63,35 +46,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   heading: {
-    fontSize: 24,
+    fontSize: 40,
     fontWeight: 'bold',
-    paddingRight: 24,
-    paddingBottom: 4,
+    paddingBottom: 20,
     color: 'white',
     fontFamily: 'Manrope_800ExtraBold',
+    textAlign: 'center',
   },
-  text: {
-    fontSize: 20,
+  subHeading: {
+    fontSize: 24,
     fontWeight: 'normal',
-    paddingBottom: 4,
-    paddingRight: 24,
+    paddingBottom: 20,
     color: 'white',
     fontFamily: 'Manrope_400Regular',
-  },
-  button: {
-    width: 300,
-    backgroundColor: '#F4B400',
-    borderRadius: 30,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    marginTop: 20,
-    alignSelf: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold',
     textAlign: 'center',
+  },
+  input: {
+    fontSize: 35,
+    fontWeight: 'normal',
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    textAlign: 'center',
+    marginBottom: 20,
   },
 });
 
