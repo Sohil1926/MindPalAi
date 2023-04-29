3b6724d3417753332f1c5513a18b33ca14f4aaec
Andrew Yang <andy.tubeee@gmail.com>
2023-04-28 21:38:52 -0400

    added back journal cover precheck

--- pages/JournalCover.js
+++ pages/JournalCover.js
@@ -68,8 +68,8 @@ const JournalCover = ({ navigation, setShowOnboarding, route }) => {
   };
 
   const generateImage = async () => {
-    // const precheck = await precheckBeforeGenerate();
-    const precheck = true; // for testing
+    const precheck = await precheckBeforeGenerate();
+    // const precheck = true; // for testing
     if (!precheck) {
       alert(
         'You already generated a journal cover for today, please come back tomorrow to generate a new one.'
