ad47e674d858b8ccda9d1dae1d6f9b96923ea53e
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2023-04-29 15:48:37 -0400

    journal archive UI changes

--- pages/JournalArchive.js
+++ pages/JournalArchive.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   StyleSheet,
   Alert,
+  Image,
 } from 'react-native';
 import { Button, Input } from '@rneui/themed';
 import axios from 'axios';
@@ -25,15 +26,22 @@ export default function JournalArchive({ navigation }) {
   });
 
   const [journalKeys, setJournalKeys] = useState([]);
-
+  const last14Days = [];
+  for (let i = 13; i >= 0; i--) {
+    const date = new Date();
+    date.setDate(date.getDate() - i);
+    last14Days.push(date);
+  }
+  
   const getAllJournals = async () => {
     try {
-      const allJournalKeys = await getAllValuesFromKey('journals');
-      if (allJournalKeys !== null) {
-        const keys = allJournalKeys?.map((j) => j.date);
-        // sort keys from newest to oldest
-        keys.sort((a, b) => new Date(b) - new Date(a));
-        setJournalKeys(keys);
+      const allJournals = await getAllValuesFromKey('journals');
+      if (allJournals !== null) {
+        const journals = {};
+        allJournals.forEach((j) => {
+          journals[j.date] = { image: j.image, source: j.source };
+        });
+        setJournalKeys(journals);
       }
     } catch (error) {
       Alert.alert(
@@ -42,6 +50,8 @@ export default function JournalArchive({ navigation }) {
       );
     }
   };
+  
+  
 
   useEffect(() => {
     // delete all journals
@@ -53,34 +63,81 @@ export default function JournalArchive({ navigation }) {
 
   return (
     <View style={styles.container}>
-      {journalKeys.length === 0 && (
-        <Text style={styles.emptyMessage}>
-          You haven't written any journals yet.
-        </Text>
+    <Text style={styles.header}>Your Journals</Text>
+    <View style={styles.box}>
+  <Text style={styles.boxHeader}>Last 14 Days</Text>
+  <View style={styles.calendar}>
+  {last14Days.map((date) => (
+  <TouchableOpacity key={date} style={styles.calendarItem}>
+  <Text style={styles.calendarDate}>{date.getDate()}</Text>
+  {journalKeys[date.toDateString()] ? (
+    <Image
+      source={journalKeys[date.toDateString()].image}
+      style={styles.calendarImage}
+    />
+  ) : (
+    <View style={styles.calendarPlaceholder} />
+  )}
+</TouchableOpacity>
+
+  ))}
+</View>
+
+</View>
+
+
+    {journalKeys.length === 0 && (
+      <Text style={styles.emptyMessage}>
+        You haven't written any journals yet.
+      </Text>
+    )}
+    <FlatList
+      data={journalKeys}
+      keyExtractor={(item) => item}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate('JournalEntry', { key: item })}
+          style={styles.journalItemContainer}
+        >
+          <Text style={styles.journalItemText}>{item}</Text>
+        </TouchableOpacity>
       )}
-      <FlatList
-        data={journalKeys}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            onPress={() => navigation.navigate('JournalEntry', { key: item })}
-            style={styles.journalItemContainer}
-          >
-            <Text style={styles.journalItemText}>{item}</Text>
-          </TouchableOpacity>
-        )}
-      />
-    </View>
+    />
+  </View>
+  
   );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: '40%',
     flex: 1,
     backgroundColor: '#000',
     paddingVertical: 20,
     paddingHorizontal: 10,
+  },
+  header: {
+    fontFamily: 'Manrope_800ExtraBold',
+    fontSize: 32,
+    color: '#fff',
+    marginBottom: 20,
+  },
+  box: {
+    backgroundColor: '#333',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  boxHeader: {
+    fontFamily: 'Manrope_800ExtraBold',
+    fontSize: 24,
+    color: '#fff',
+    marginBottom: 10,
+  },
+  calendar: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
   },
   emptyMessage: {
     fontFamily: 'Manrope_400Regular',
@@ -103,4 +160,33 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#fff',
   },
+  calendar: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  calendarItem: {
+    alignItems: 'center',
+    width: '14%',
+    aspectRatio: 1,
+  },
+  calendarDate: {
+    fontFamily: 'Manrope_400Regular',
+    fontSize: 18,
+    color: '#fff',
+    marginBottom: 10,
+  },
+  calendarImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  calendarPlaceholder: {
+    width: 40,
+    height: 40,
+    backgroundColor: '#555',
+    borderRadius: 20,
+  },
+  
 });
