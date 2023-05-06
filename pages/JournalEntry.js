52573942bf9dc3c68d9da41a309c058610d42f7b
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2023-05-06 14:09:39 -0400

    changed journal entry styling

--- pages/JournalEntry.js
+++ pages/JournalEntry.js
@@ -1,146 +1,183 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, FlatList, Alert, ScrollView } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import { Button, Input } from '@rneui/themed';
-import axios from 'axios';
-import qs from 'qs';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {
-  deleteValueFromArr,
-  getAllValuesFromKey,
-  getValueUsingFieldVal,
-} from '../utils/asyncStorageUtils';
-import PillButton from '../components/PillButton';
-import { Image } from '@rneui/base';
+  import { StatusBar } from 'expo-status-bar';
+  import { Text, View, FlatList, Alert, ScrollView, StyleSheet} from 'react-native';
+  import React, { useState, useEffect } from 'react';
+  import { Button, Input } from '@rneui/themed';
+  import axios from 'axios';
+  import qs from 'qs';
+  import AsyncStorage from '@react-native-async-storage/async-storage';
+  import {
+    deleteValueFromArr,
+    getAllValuesFromKey,
+    getValueUsingFieldVal,
+  } from '../utils/asyncStorageUtils';
+  import PillButton from '../components/PillButton';
+  import { Image } from '@rneui/base';
+  import {
+    useFonts,
+    Manrope_800ExtraBold,
+    Manrope_400Regular,
+    Manrope_200ExtraLight,
+    Manrope_300Light,
+    Manrope_500Medium,
+    Manrope_600SemiBold,
+    Manrope_700Bold,
+  } from '@expo-google-fonts/manrope';
+  // import { createCompletion } from './openAI';
 
-// import { createCompletion } from './openAI';
-export default function JournalArchive({ navigation, route }) {
-  const [journalEntry, setJournalEntry] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
 
-  const getJournalEntry = async () => {
-    try {
-      // outdated method
-      // const journal = await getValueUsingFieldVal(
-      //   'journals',
-      //   'date',
-      //   route.params.key
-      // );
-
-      const allJournals = await getAllValuesFromKey('journals');
-      const journal = allJournals.find((j) => {
-        const jDate = new Date(j.date).toISOString().split('T')[0];
-        return jDate === route.params.key;
-      });
-
-      // console.log(journal.journalCover);
-      setJournalEntry(journal.entry);
-
-      setImageUrl(
-        journal.journalCover ||
-          'https://thumbs.dreamstime.com/b/letter-block-word-null-wood-background-187721938.jpg'
-      );
-    } catch (e) {
-      // error reading value
-      Alert.alert('Error', 'Something unexpected happened. Please try again.');
-    }
-  };
-
-  const generateImage = async () => {
-    setImageUrl('loading');
-
-    let data = qs.stringify({
-      input: `This is my's journal entry: "${journalEntry}"
-      I want an image that represents my mood.`,
+  
+  export default function JournalArchive({ navigation, route }) {
+    const [fontsLoaded] = useFonts({
+      Manrope_800ExtraBold,
+      Manrope_400Regular,
+      Manrope_200ExtraLight,
+      Manrope_300Light,
+      Manrope_500Medium,
+      Manrope_600SemiBold,
+      Manrope_700Bold,
     });
-    let config = {
-      method: 'post',
-      maxBodyLength: Infinity,
-      url: 'https://openaibackend-nt11.onrender.com/dalle',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      data,
+      
+    const [journalEntry, setJournalEntry] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+
+    const getJournalEntry = async () => {
+      try {
+        // outdated method
+        // const journal = await getValueUsingFieldVal(
+        //   'journals',
+        //   'date',
+        //   route.params.key
+        // );
+
+        const allJournals = await getAllValuesFromKey('journals');
+        const journal = allJournals.find((j) => {
+          const jDate = new Date(j.date).toISOString().split('T')[0];
+          return jDate === route.params.key;
+        });
+
+        // console.log(journal.journalCover);
+        setJournalEntry(journal.entry);
+
+        setImageUrl(
+          journal.journalCover ||
+            'https://thumbs.dreamstime.com/b/letter-block-word-null-wood-background-187721938.jpg'
+        );
+      } catch (e) {
+        // error reading value
+        Alert.alert('Error', 'Something unexpected happened. Please try again.');
+      }
     };
 
-    axios
-      .request(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-        setImageUrl(response.data.response);
-      })
-      .catch((error) => {
-        Alert.alert('Error', String(error));
-        console.error(error);
+    const generateImage = async () => {
+      setImageUrl('loading');
+
+      let data = qs.stringify({
+        input: `This is my's journal entry: "${journalEntry}"
+        I want an image that represents my mood.`,
       });
-  };
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://openaibackend-nt11.onrender.com/dalle',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        data,
+      };
 
-  const delEntry = async () => {
-    try {
-      await deleteValueFromArr('journals', 'date', route.params.key);
-      navigation.navigate('JournalArchive');
-    } catch (e) {
-      // error reading value
-      Alert.alert(
-        'Error',
-        'Something unexpected happened. Please try again later.'
-      );
-    }
-  };
+      axios
+        .request(config)
+        .then((response) => {
+          console.log(JSON.stringify(response.data));
+          setImageUrl(response.data.response);
+        })
+        .catch((error) => {
+          Alert.alert('Error', String(error));
+          console.error(error);
+        });
+    };
 
-  useEffect(() => {
-    getJournalEntry();
-  });
+    const delEntry = async () => {
+      try {
+        await deleteValueFromArr('journals', 'date', route.params.key);
+        navigation.navigate('JournalArchive');
+      } catch (e) {
+        // error reading value
+        Alert.alert(
+          'Error',
+          'Something unexpected happened. Please try again later.'
+        );
+      }
+    };
 
-  return (
-    <View className='flex-1 bg-black pt-20'>
-      <Text
-        style={{
-          textAlign: 'left',
-          fontSize: 20,
-          fontWeight: 'bold',
-          color: 'white',
-          paddingTop: 30,
-          paddingLeft: 30,
-        }}
-      >
-        {route.params.key}
-      </Text>
+    useEffect(() => {
+      getJournalEntry();
+    });
+    if (!fontsLoaded) return null;
 
-      <ScrollView style={{ borderColor: 'white', border }}>
-        <Text style={{ color: 'white', paddingLeft: '15%' }}>
-          {journalEntry}
-        </Text>
-      </ScrollView>
+    return (
+      
+      <View className='flex-1 bg-black pt-20'>
+        
+          <Text style={styles.heading}>InnerScribe.</Text>
 
-      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-        <Image
-          source={{ uri: imageUrl }}
+        <Text
           style={{
-            width: 300,
-            height: 300,
-            borderRadius: 20,
+            textAlign: 'left',
+            fontSize: 20,
+            color: 'white',
+            paddingTop: 30,
+            paddingLeft: 30,
+            fontFamily: 'Manrope_700Bold',
+            paddingBottom: '5%',
           }}
-        />
-      </View>
+        >
+          {route.params.key}
+        </Text>
 
-      <View style={{ marginBottom: 30 }}>
-        <PillButton
-          text={'Delete'}
-          onPress={delEntry}
-          textColor='white'
-          bgColor='#ff0f00'
-        />
-      </View>
-    </View>
-  );
-}
+        <ScrollView style={{ borderColor: 'white'  }}>
+          <Text style={{ color: 'white', paddingLeft: '9%', fontFamily: 'Manrope_400Regular', fontSize: 16,}}>
+            {journalEntry}
+          </Text>
+        </ScrollView>
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <Image
+            source={{ uri: imageUrl }}
+            style={{
+              width: 300,
+              height: 300,
+              borderRadius: 20,
+              marginTop: 50,
+              marginBottom: 20,
+            }}
+          />
+        </View>
+
+        <View style={{ marginBottom: 30 }}>
+          <PillButton
+            text={'Delete'}
+            onPress={delEntry}
+            textColor='white'
+            bgColor='#ff0f00'
+          />
+        </View>
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  heading: {
+      fontSize: 20,
+      marginTop: '-10%',
+      color: 'white',
+      fontFamily: 'Manrope_800ExtraBold',
+      textAlign: 'center',
+    },
+  });
